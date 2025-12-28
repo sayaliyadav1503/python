741b00a797811b29c4be0b85d72d17efe26ad55b
rohit yadav <ry564919@gmail.com>
2025-12-28 14:02:50 +0530

    modified...

--- StringReversal.py
+++ StringReversal.py
@@ -1,8 +1,17 @@
-s = "Sayaliyadav"
-rev=s[::-1]
-print(rev[3])
+s = "sayali"
+rev1 = s[::-1]
+print(rev1)
 
-s = "developer"
+#reversed from 3rd character
 
-rev = ''.join(reversed(s))
-print(rev[3])
+S = "sayalii"
+rev=S[:2]+S[2:][::-1]
+print(rev)
+
+#using loop
+
+s="python"
+rev2=" "
+for i in s:
+    rev2=i+rev2
+print(rev2)
