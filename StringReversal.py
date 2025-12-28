9f01f9173c55dc366f9ec18cebc0b5af6972e2d3
rohit yadav <ry564919@gmail.com>
2025-12-28 13:36:37 +0530

    updated...

---
+++ StringReversal.py
@@ -0,0 +1,8 @@
+s = "Sayaliyadav"
+rev=s[::-1]
+print(rev[3])
+
+s = "developer"
+
+rev = ''.join(reversed(s))
+print(rev[3])
---
+++ list1.py
@@ -0,0 +1,10 @@
+list1=[2,4,5]
+list2=[5,1,8]
+list3=list1+list2
+list3.sort()
+print(list3)
+
+list1=[1,2,3,5,6,7,8,9,3,5,6]
+unique=set(list(list1))
+print(list1)
+
