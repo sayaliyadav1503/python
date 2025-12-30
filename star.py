2584c5a7bcca02fb1823089cdc158b8d41277890
rohit yadav <ry564919@gmail.com>
2025-12-31 00:12:33 +0530

    modified..

--- star.py
+++ star.py
@@ -9,14 +9,8 @@ for i in range(n):
       print("*" *n)
 print("\n")
 
-#user input
-s=8
-for i in range(0,s):
-       print("*"*i)
-print("\n")
-print("\n")
-
 #inverted
+s=5
 for i in range(s,0,-1):
       print("*"*i)
 print("\n")
@@ -26,9 +20,15 @@ n=10
 for i in range(1,n):
       print(" " * (n-i) + "*" * (2*i-1))
 print("\n")
-   
+
+#inverted piramid   
 n=10
 for i in range(n,0,-1):
       print(" " * (n-i) + "*" * (2*i-1))    
 print("\n")
 
+n=4
+for i in range(1,n):
+      print(" " * (n-i) + "*" * (2*i-1)) 
+for i in range(n,0,-1):
+      print(" " * (n-i) + "*" * (2*i-1)) 
